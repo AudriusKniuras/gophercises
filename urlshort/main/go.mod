56d99e55345a4ee36db8caa820041714aef26e27
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-06-30 20:54:25 +0300

    f

--- urlshort/main/go.mod
+++ urlshort/main/go.mod
@@ -2,4 +2,4 @@ module github.com/AudriusKniuras/gophercises/urlshort/main
 
 go 1.16
 
-require github.com/AudriusKniuras/gophercises v0.0.0-20210630173519-1afa2484cd9f // indirect
+require github.com/AudriusKniuras/gophercises v0.0.0-20210630174920-658f38ad6256 // indirect
--- urlshort/urlshort.go
+++ urlshort/urlshort.go
@@ -1,13 +1,11 @@
 package urlshort
 
-func MapHandler() string {
-	return "hello"
+import "net/http"
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return nil
 }
 
-// func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-// 	return nil
-// }
-
-// func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-// 	return nil, nil
-// }
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	return nil, nil
+}
